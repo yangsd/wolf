d9e46379462cbd855f390fccafb8ebdd01ddf50d
yangsd <1289668604@qq.com>
2016-11-05 00:09:08 +0800

    修复错误

--- src/main/java/com/wolf/auth/mapper/RoleResourceMapper.java
+++ src/main/java/com/wolf/auth/mapper/RoleResourceMapper.java
@@ -1,5 +1,6 @@
 package com.wolf.auth.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
@@ -8,6 +9,7 @@ import java.util.List;
 /**
  * Created by sdyang on 2016/11/4.
  */
+@Mapper
 public interface RoleResourceMapper {
 
     @Select("select rr.id from auth_role r left join auth_role_resource rr on r.id = rr.role_id" +
--- src/main/java/com/wolf/auth/mapper/UserRoleMapper.java
+++ src/main/java/com/wolf/auth/mapper/UserRoleMapper.java
@@ -1,6 +1,7 @@
 package com.wolf.auth.mapper;
 
 import com.wolf.auth.model.UserRole;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
@@ -9,6 +10,7 @@ import java.util.List;
 /**
  * Created by sdyang on 2016/11/4.
  */
+@Mapper
 public interface UserRoleMapper {
 
     @Select("select id,user_id,role_id from auth_user_role where user_id=#{user_id}")
